c5df2199ef512bc1aa52be7edeb5ffee4206e100
Geeballer <jibola422@gmail.com>
2025-07-13 17:30:41 +0100

    -- chore: remove all tavily related code

--- config/index.py
+++ config/index.py
@@ -4,11 +4,7 @@ import os
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 MODEL = os.getenv("MODEL")
 
 if not OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY not found in .env file.")
-
-if not TAVILY_API_KEY:
-    raise ValueError("TAVILY_API_KEY not found in .env file.")
